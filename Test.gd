3037291da1630c650303d1bd2328016b14b0843f
Thebarda <tomdar87@outlook.com>
2023-11-20 19:39:57 +0100

    Clean up animation

--- Test.gd
+++ Test.gd
@@ -1,5 +1,16 @@
 extends Node2D
 
+var is_attacking = false
+
 func _ready():
 	$Sprite2D/AnimationPlayer.play("new_animation")
 	$Marker2D/AnimationPlayer.play("new_animation")
+	
+func _process(delta):
+	if Input.is_action_pressed("Attack"):
+		is_attacking = true
+	else:
+		is_attacking = false
+
+func get_is_attacking():
+	return is_attacking
--- any_object.gd
+++ any_object.gd
@@ -5,6 +5,8 @@ extends Node2D
 func _process(delta):
 	if (player == null || player.has_node("Marker2D") == false):
 		print("No player attached")
-		
+	
 	global_position = player.get_node("Marker2D").global_position
 	global_rotation = player.get_node("Marker2D").global_rotation
+	
+	print(player.get_is_attacking())
